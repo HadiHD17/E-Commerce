625fbb30512759af9b3323c6ed764dbe50115b3e
Abdullah Z <dev@zabd.dev>
2025-07-31 01:42:03 +0300

    refactor(frontend): move tests/ dir to frontend root

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,6 +15,6 @@ export default defineConfig({
     test: {
         environment: "jsdom",
         globals: true,
-        setupFiles: "./src/tests/setup.js",
+        setupFiles: "./tests/setup.js",
     },
 });
