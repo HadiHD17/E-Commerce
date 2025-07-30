73a837f2cb3595211e341009018070da03517fbd
HadiHD17 <hadihaidar1723@gmail.com>
2025-07-30 19:39:24 +0300

    Monorepo setup: moved client and server projects

---
+++ client/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["react-app", "eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    // optional: custom rules
+    "no-unused-vars": "warn",
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+  },
+};
