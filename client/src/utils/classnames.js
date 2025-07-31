c3460ec419e8aef47ed5650141b4dcc29efb6ec6
Abdullah Z <dev@zabd.dev>
2025-07-31 21:20:05 +0300

    feat(frontend): create input component
    * add comment for cls() util
    * add 2 new icons: `eye` & `eye-closed`

--- client/src/utils/classnames.js
+++ client/src/utils/classnames.js
@@ -1,3 +1,6 @@
+/**
+ * Utility function to join a list of CSS classes into one string
+ */
 export default function cls(...classes) {
     // `filter(Boolean)` filters out falsy values,
     return classes.filter(Boolean).join(" ");
