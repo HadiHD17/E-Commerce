163806cea60fb6d0f0ff5914dc9ea73a40e9db39
HadiHD17 <hadihaidar1723@gmail.com>
2025-08-05 00:40:31 +0300

    added folder

--- client/src/pages/account/my-orders/index.jsx
+++ client/src/pages/account/my-orders/index.jsx
@@ -3,38 +3,7 @@ import OrderCard from "@/components/shared/order-card";
 import "./my-orders.css";
 
 export default function MyOrdersPage() {
-    const ordersData = [
-        {
-            image: "/test-images/688a90f995ca1_test_photo.jpg",
-            productName: "NVIDIA GeForce RTX 4070",
-            quantity: 1,
-            price: 399.32,
-            address: "123 abc st., Bchamoun, Lebanon",
-            orderId: "1234-4321-01",
-            statusClass: "green",
-            statusText: "Arrives in 1 day",
-        },
-        {
-            image: "/test-images/688a90f995ca1_test_photo.jpg",
-            productName: "NVIDIA GeForce RTX 4070",
-            quantity: 1,
-            price: 399.32,
-            address: "123 abc st., Bchamoun, Lebanon",
-            orderId: "1234-4321-02",
-            statusClass: "gray",
-            statusText: "Delivered",
-        },
-        {
-            image: "/test-images/688a90f995ca1_test_photo.jpg",
-            productName: "NVIDIA GeForce RTX 4070",
-            quantity: 1,
-            price: 399.32,
-            address: "123 abc st., Bchamoun, Lebanon",
-            orderId: "1234-4321-03",
-            statusClass: "red",
-            statusText: "Cancelled",
-        },
-    ];
+    const ordersData;
 
     return (
         <div className="orders-container">
