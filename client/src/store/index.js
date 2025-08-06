f2953374e52677df01ed2eef1f6e3f4960c66740
HadiHD17 <hadihaidar1723@gmail.com>
2025-08-06 12:18:45 +0300

    fix

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./slices/products-slice";
 import authReducer from "./slices/auth-slice";
 import ordersReducer from "./slices/orders-slice";
-import userReducer from "./slices/user-slice";
 import cartReducer from "./slices/cart-slice";
 
 export default configureStore({
@@ -10,7 +9,6 @@ export default configureStore({
         products: productsReducer,
         orders: ordersReducer,
         auth: authReducer,
-        user: userReducer,
         cart: cartReducer,
     },
 });
